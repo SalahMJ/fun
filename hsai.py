651cd669e1cb0a88325e6b2c0082602f12b96c70
SalahMJ <70417097+SalahMJ@users.noreply.github.com>
2020-10-11 15:29:29 +0530

    Update hsai.py

--- hsai.py
+++ hsai.py
@@ -5,12 +5,16 @@ from colored import fg , attr
 
 col1 = fg('red')
 col2 = fg('green')
+col3 = fg('blue')
 reset = attr('reset')
-x = ('HAMZAH' , 'hamzah' , 'SABA' , 'saba' , 'Saba' , 'Hamzah' ,'Sarim' , 'SARIM' , 'sarim')
+x = ('HAMZAH' , 'hamzah' , 'Hamzah' ,'Sarim' , 'SARIM' , 'sarim')
+y = ('SALAH' , 'Salah' , 'salah')
 
 n=str(input('ENTER YOUR NAME:'))
 
 if n in x:
-   print(col1,'BLOODY MORONNNN',reset)
+	print(col1,'BLOODY MORONNNN',reset)
+elif n in y:
+	print(col2,'SHAREEF',reset)
 else:
-   print(col2,'SHAREEF',reset)
+	print(col3,'I DON\'T KNOW HIM',reset)
